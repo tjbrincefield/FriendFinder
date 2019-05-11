b26a806b69feda28716d96c33aff065b4a2ef773
Tiffany B <tjbrincefield@gmail.com>
2019-05-10 21:53:51 -0400

    updating survey questions and logic

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,9 +2,8 @@
 module.exports = function(app) {
   // ---------------------------------------------------------------------------
 
-  app.get("/", function(req, res) {
+  app.get("/survey", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/survey.html"));
-    // Why is this not routing to the survey when the button is clicked??
   });
 
   app.get("*", function(req, res) {
