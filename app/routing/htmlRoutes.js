914584e9f64469e94de767602b0db415c3cd29a6
Tiffany B <tjbrincefield@gmail.com>
2019-05-10 21:48:53 -0400

    updated routing to link to survey

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,6 +4,7 @@ module.exports = function(app) {
 
   app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/survey.html"));
+    // Why is this not routing to the survey when the button is clicked??
   });
 
   app.get("*", function(req, res) {
