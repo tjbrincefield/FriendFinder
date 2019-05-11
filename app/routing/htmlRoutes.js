a4f8d35357f7a6b2a7ab3c0a765bab654b5512ae
Tiffany B <tjbrincefield@gmail.com>
2019-05-10 21:46:49 -0400

    updating html routes to get to the survey

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,7 +2,7 @@
 module.exports = function(app) {
   // ---------------------------------------------------------------------------
 
-  app.get("/survey", function(req, res) {
+  app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
 
